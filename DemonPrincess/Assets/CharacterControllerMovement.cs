7df4335bba38a73a479bd8a679ef9917e87c54e9
Charles <charleswcary@gmail.com>
2017-06-05 21:21:27 -0600

    Character controller and animation stuff

--- DemonPrincess/Assets/CharacterControllerMovement.cs
+++ DemonPrincess/Assets/CharacterControllerMovement.cs
@@ -5,22 +5,48 @@ using UnityEngine;
 public class CharacterControllerMovement : MonoBehaviour {
 
     public Rigidbody character;
+    public Animator anim;
 
-    float moveSpeed = 10f;
+    float moveSpeed = 20f;
     float jumpForce = 5f;
+    float stopFast = .9f;
 
     bool isGrounded = true;
-
     
+    private float floAnimJump;
+    private float floAnimJumpLeg;
+    private bool booAnimCrouch;
+
 
     void FixedUpdate()
     {
-        Vector3 moveTarget = (Input.GetAxis("Vertical") * Camera.main.transform.forward + Input.GetAxis("Horizontal") * Camera.main.transform.right); 
+        Vector3 floFinalMove;
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        if (moveZ > 0) { moveZ += 1; } else if(moveZ < 0) { moveZ -= 1; }
+        Vector3 camRight = Camera.main.transform.right * moveX;
+        float camForward = Camera.main.transform.forward.z * moveZ;
+        Vector3 moveTarget = new Vector3(camRight.x, 0f, camForward);
+
+        //Debug.Log(moveZ + " moveZ");
+        //Debug.Log(moveX + " moveX");
+        //Debug.Log(camForward + " camForward");
+        //Debug.Log(Camera.main.transform.forward + " camfor true");
+        //Debug.Log(camRight + " camRight");
+        //Vector3 moveTarget = (Input.GetAxis("Vertical") * Camera.main.transform.forward + Input.GetAxis("Horizontal") * Camera.main.transform.right); 
         //for some reason the vertical movement(up, down, "w" and "s", isn't giving a number large enough to move the character unless the character has horizontal movement.
         moveTarget.y = 0f;
         if (isGrounded) //can only move if on the ground, character keeps velocity in air but can't change or add velocity
         {
-            character.AddRelativeForce(moveTarget * moveSpeed * Time.deltaTime, ForceMode.Impulse); //adding force based on target position
+            floFinalMove = moveTarget * moveSpeed * Time.deltaTime;
+            //Debug.Log((moveTarget * moveSpeed * Time.deltaTime).ToString() + " Stuff");
+            anim.SetFloat("Forward", floFinalMove.z);
+            anim.SetFloat("Turn", floFinalMove.x);
+            character.AddForce(floFinalMove, ForceMode.Impulse); //adding force based on target position
+        }
+        if (moveX == 0 && moveZ == 0)
+        {
+            character.velocity *= stopFast;
         }
     }
 
@@ -41,6 +67,7 @@ public class CharacterControllerMovement : MonoBehaviour {
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
+            Debug.Log("Jump");
             character.AddRelativeForce(0, jumpForce, 0, ForceMode.VelocityChange);
         }
 
