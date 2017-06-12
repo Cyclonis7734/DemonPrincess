3fce4c1e6bad795e4263317860c01acbfdc85817
Thomas Seggerman <thomas.seggerman@gmail.com>
2017-06-12 16:15:23 -0600

    Added Goblin to scene for testing

---
+++ DemonPrincess/Assets/_Scripts/Enemy AI/EnemyS_Wandering.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyS_Wandering : EnemySMInterface
+{
+    Enemy ESM;
+
+    //Constructor
+    public EnemyS_Wandering(Enemy newESM)
+    {
+        ESM = newESM;
+    }
+
+    public void IsAttacking()
+    {
+        ESM.SetEnemyState(ESM.getIsAttacking());
+        ESM.HandleAttacking();
+    }
+
+    public void IsDead()
+    {
+        ESM.SetEnemyState(ESM.getIsDeathSet());
+        ESM.KillThisEnemy();
+    }
+
+    public void IsIdle()
+    {
+
+    }
+
+    public void IsWandering()
+    {
+        ESM.HandleWandering();
+    }
+
+    public void IsPursuing()
+    {
+        ESM.SetEnemyState(ESM.getIsPursuing());
+        ESM.PursuePlayer();
+    }
+}
